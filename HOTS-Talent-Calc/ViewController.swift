222f8573cd788f252fc739bb712bbfa5a06b731d
Greg Morano <gmorano90@gmail.com>
2017-07-27 17:53:44 -0500

    Initial Commit

---
+++ HOTS-Talent-Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HOTS-Talent-Calc
+//
+//  Created by Greg Morano on 7/27/17.
+//  Copyright © 2017 Greg Morano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
